6adeaf0cef1c159e1a6b049c1761b8946ccdc334
remi cresson <remi.cresson@teledetection.fr>
2018-09-12 15:09:34 +0200

    ADD: new python application to convert a checkpoint into a protobuf model

---
+++ python/ckpt2savedmodel.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#==========================================================================
+#
+#   Copyright Remi Cresson (IRSTEA)
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#==========================================================================*/
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+from tricks import *
+
+# Logging
+tf.logging.set_verbosity(tf.logging.INFO)
+
+# Parser
+parser = argparse.ArgumentParser()
+parser.add_argument("--ckpt",    help="checkpoint file prefix",   required=True)
+parser.add_argument("--inputs",  help="input placeholder names",  required=True, nargs='+')
+parser.add_argument("--outputs", help="output placeholder names", required=True, nargs='+')
+parser.add_argument("--model",   help="output SavedModel",        required=True)
+params = parser.parse_args()
+
+if __name__ == "__main__":
+
+  CheckpointToSavedModel(params.ckpt, params.inputs, params.outputs, params.model)
+  
+  quit()
