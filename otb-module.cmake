85841d340346531ba6eaafeaac80220605622a2a
remi <remi.cresson@irstea.fr>
2019-09-26 08:20:28 +0000

    COMP: update cmake files to build against latest OTB develop

--- otb-module.cmake
+++ otb-module.cmake
@@ -2,19 +2,19 @@ set(DOCUMENTATION "An unofficial module enabling to play with Tensorflow")
 
 # define the dependencies of the include module and the tests
 otb_module(OTBTensorflow
-	DEPENDS
-		OTBCommon
-		OTBApplicationEngine
-		OTBStreaming
-		OTBExtendedFilename
-		OTBImageIO
-		OTBSupervised
+  DEPENDS
+    OTBCommon
+    OTBApplicationEngine
+    OTBStreaming
+    OTBExtendedFilename
+    OTBImageIO
+    OTBSupervised
     OTBIOXML
     OTBConversion
     OTBStatistics
-	TEST_DEPENDS
-		OTBTestKernel
-		OTBCommandLine
-	DESCRIPTION
-		"${DOCUMENTATION}"
+  TEST_DEPENDS
+    OTBTestKernel
+    OTBCommandLine
+  DESCRIPTION
+    "${DOCUMENTATION}"
 )
