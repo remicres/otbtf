75323aef7a791ec344b1dcb0651edb79863b3afe
remi <remi.cresson@irstea.fr>
2019-10-29 14:14:03 +0100

    ENH: fix bug with nodata

--- include/otbTensorflowSampler.hxx
+++ include/otbTensorflowSampler.hxx
@@ -215,19 +215,19 @@ TensorflowSampler<TInputImage, TVectorData>
       } // Next input
       if (hasBeenSampled)
       {
-        // TODO: delete this
-        IndexType outIndex;
-        outIndex[0] = 0;
-        outIndex[1] = count * m_PatchSizes[0][1];
-        RegionType region(outIndex, m_PatchSizes[0]);
-        IteratorType it(m_OutputPatchImages[0], region);
-        for (it.GoToBegin(); !it.IsAtEnd(); ++it)
-          {
-          PixelType pix = it.Get();
-          for (unsigned int band; band < pix.Size(); band++)
-            if (pix[band] == m_NodataValue)
-              std::cout << band << std::endl;
-          }
+//        // TODO: delete this
+//        IndexType outIndex;
+//        outIndex[0] = 0;
+//        outIndex[1] = count * m_PatchSizes[0][1];
+//        RegionType region(outIndex, m_PatchSizes[0]);
+//        IteratorType it(m_OutputPatchImages[0], region);
+//        for (it.GoToBegin(); !it.IsAtEnd(); ++it)
+//          {
+//          PixelType pix = it.Get();
+//          for (unsigned int band; band < pix.Size(); band++)
+//            if (pix[band] == m_NodataValue)
+//              std::cout << band << std::endl;
+//          }
 
         // Fill label
         labelIndex[1] = count;
