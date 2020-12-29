7b0ce95000ec9eb867bb8dcb566117ab605ac85e
Vincent Delbar <vincent.delbar@latelescop.fr>
2020-12-29 22:39:31 +0100

    FIX: replace gitlab by mdl4eo docker repository

--- tools/docker/multibuild.sh
+++ tools/docker/multibuild.sh
@@ -13,24 +13,23 @@ mkdir -p $HOME/.cache/bazel
 docker run -d -u 1000:1000 -v $HOME/.cache/bazel:/data -p 9090:8080  buchgr/bazel-remote-cache --max_size=20
 
 # GPU support is enabled if CUDA is found in /usr/local
-docker build --network='host' -t registry.gitlab.com/latelescop/docker/otbtf:r$RELEASE:gpu --build-arg BASE_IMG=$GPU_IMG .
+docker build --network='host' -t mdl4eo/otbtf$RELEASE:gpu --build-arg BASE_IMG=$GPU_IMG .
 # Keep OTB src and build files in order to rebuild with other modules
-docker build --network='host' -t registry.gitlab.com/latelescop/docker/otbtf:r$RELEASE:gpu-dev --build-arg BASE_IMG=$GPU_IMG --build-arg KEEP_SRC_OTB=true .
+docker build --network='host' -t mdl4eo/otbtf$RELEASE:gpu-dev --build-arg BASE_IMG=$GPU_IMG --build-arg KEEP_SRC_OTB=true .
 
 # CPU
-docker build --network='host' -t registry.gitlab.com/latelescop/docker/otbtf:r$RELEASE:cpu --build-arg BASE_IMG=$IMG .
-docker build --network='host' -t registry.gitlab.com/latelescop/docker/otbtf:r$RELEASE:cpu-dev --build-arg BASE_IMG=$IMG --build-arg KEEP_SRC_OTB=true .
+docker build --network='host' -t mdl4eo/otbtf$RELEASE:cpu --build-arg BASE_IMG=$IMG .
+docker build --network='host' -t mdl4eo/otbtf$RELEASE:cpu-dev --build-arg BASE_IMG=$IMG --build-arg KEEP_SRC_OTB=true .
 # Enable MKL with bazel config flag
 #BZL_CONFIG="--config=opt --config=nogcp --config=noaws --config=nohdfs --config=mkl --copt='-mfpmath=both'"
-#docker build --network='host' -t registry.gitlab.com/latelescop/docker/otbtf:r$RELEASE:cpu-mkl --build-arg BASE_IMG=$IMG --build-arg BZL_CONFIG=$CONF .
+#docker build --network='host' -t mdl4eo/otbtf$RELEASE:cpu-mkl --build-arg BASE_IMG=$IMG --build-arg BZL_CONFIG=$CONF .
 
-docker login registry.gitlab.com
+#docker login
+docker push mdl4eo/otbtf$RELEASE:cpu
+docker push mdl4eo/otbtf$RELEASE:cpu-dev
+#docker push mdl4eo/otbtf$RELEASE:cpu-mkl
+#docker push mdl4eo/otbtf$RELEASE:cpu-gui
 
-docker push registry.gitlab.com/latelescop/docker/otbtf:r$RELEASE:cpu
-docker push registry.gitlab.com/latelescop/docker/otbtf:r$RELEASE:cpu-dev
-#docker push registry.gitlab.com/latelescop/docker/otbtf:r$RELEASE:cpu-mkl
-#docker push registry.gitlab.com/latelescop/docker/otbtf:r$RELEASE:cpu-gui
-
-docker push registry.gitlab.com/latelescop/docker/otbtf:r$RELEASE:gpu
-docker push registry.gitlab.com/latelescop/docker/otbtf:r$RELEASE:gpu-dev
-#docker push registry.gitlab.com/latelescop/docker/otbtf:r$RELEASE:gpu-gui
+docker push mdl4eo/otbtf$RELEASE:gpu
+docker push mdl4eo/otbtf$RELEASE:gpu-dev
+#docker push mdl4eo/otbtf$RELEASE:gpu-gui
