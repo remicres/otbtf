9756a0c45af501658c66328f6135319c8165725b
Narcon Nicolas <nicolas.narcon@inrae.fr>
2021-08-30 09:57:06 +0200

    FIX: remove obsolete code and handle the :0 at the end of a layer name

--- include/otbTensorflowMultisourceModelFilter.hxx
+++ include/otbTensorflowMultisourceModelFilter.hxx
@@ -394,31 +394,6 @@ TensorflowMultisourceModelFilter<TInputImage, TOutputImage>
   DictType inputs;
 
 
-  // DEBUG Tentative
-  auto outs = this->GetSignatureDef().outputs();
-  for (auto& output: outs)
-  {
-    std::string name = output.first;
-    std::string layerName = output.second.name();
-    //m_UserNameToLayerNameMapping[name] = layerName;
-    std::cout << "DEBUG dans ModelFiler.hxx : outs pour remplir mapping " << name << std::endl;
-  }
-  for (auto& output: this->GetSignatureDef().outputs())
-  {
-    std::string name = output.first;
-    std::string layerName = output.second.name();
-    //m_UserNameToLayerNameMapping[name] = layerName;
-    std::cout << "DEBUG dans ModelFiler.hxx : this->GetSignatureDef().outputs() pour remplir mapping " << name << std::endl;
-  } 
-  for (auto& input: this->GetSignatureDef().inputs())
-  {
-    std::string inputName = input.first;
-    std::string layerName = input.second.name();
-    //m_UserNameToLayerNameMapping[inputName] = layerName;
-    std::cout << "DEBUG dans ModelFilter.hxx: this->GetSignatureDef().inputs() pour remplir mapping " << inputName << std::endl;
-  } 
-
-
   // Populate input tensors
   for (unsigned int i = 0 ; i < nInputs ; i++)
     {
