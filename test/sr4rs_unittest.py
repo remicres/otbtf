f27640a6f0286e3a9228132ae2979181c6ea6f32
benjamin.commandre <benjamin.commandre@irstea.fr>
2021-10-08 13:49:25 +0200

    Test

--- test/sr4rs_unittest.py
+++ test/sr4rs_unittest.py
@@ -17,19 +17,19 @@ class SR4RSTest(unittest.TestCase):
                   "--savedmodel /builds/remi.cresson/sr4rs_sentinel2_bands4328_france2020_savedmodel/ "
                   "--output '/tmp/sr4rs.tif?&box=256:256:512:512'")
 
-        with gdal.Open("/tmp/sr4rs.tf") as reconstruct:
+        with gdal.Open("/tmp/sr4rs.tif") as reconstruct:
             nbchannels_reconstruct = reconstruct.RasterCount
 
-        with gdal.Open("/builds/remi.cresson/sr4rs_data/baseline/sr4rs.tf") as baseline:
+        with gdal.Open("/builds/remi.cresson/sr4rs_data/baseline/sr4rs.tif") as baseline:
             nbchannels_baseline = baseline.RasterCount
 
         self.assertTrue(nbchannels_reconstruct == nbchannels_baseline)
 
         for i in range(1, 1+nbchannels_baseline):
             comp = otb.Registry.CreateApplication('CompareImages')
-            comp.SetParameterString('ref.in', "/builds/remi.cresson/sr4rs_data/baseline/sr4rs.tf")
+            comp.SetParameterString('ref.in', "/builds/remi.cresson/sr4rs_data/baseline/sr4rs.tif")
             comp.SetParameterInt('ref.channel', i)
-            comp.SetParameterString('meas.in', "/tmp/sr4rs.tf")
+            comp.SetParameterString('meas.in', "/tmp/sr4rs.tif")
             comp.SetParameterInt('meas.channel', i)
             comp.Execute()
             mae = comp.GetParameterFloat('mae')
