a8fd9a6a488c9eb430e2dca0cb41165366463fa9
remi cresson <remi.cresson@inrae.fr>
2021-10-14 17:36:02 +0200

    DOC: update copyright header

--- python/ckpt2savedmodel.py
+++ python/ckpt2savedmodel.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # ==========================================================================
 #
-#   Copyright 2018-2019 Remi Cresson (IRSTEA)
-#   Copyright 2020 Remi Cresson (INRAE)
+#   Copyright 2018-2019 IRSTEA
+#   Copyright 2020-2021 INRAE
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
