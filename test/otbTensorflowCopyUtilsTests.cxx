f82e152f0b03cb0d36e61486607d3a53415a996d
Remi Cresson <remi.cresson@irstea.fr>
2021-10-19 18:43:58 +0200

    ADD: unit tests for input vector

--- test/otbTensorflowCopyUtilsTests.cxx
+++ test/otbTensorflowCopyUtilsTests.cxx
@@ -17,15 +17,29 @@
 
 int compare(tensorflow::Tensor & t1, tensorflow::Tensor & t2)
 {
+  std::cout << "Compare " << t1 << " and " << t2 << std::endl;
   if (t1.dims() != t2.dims())
+    {
+    std::cout << "dims() differ!" << std::endl;
     return EXIT_FAILURE;
+    }
   if (t1.dtype() != t2.dtype())
+    {
+    std::cout << "dtype() differ!" << std::endl;
     return EXIT_FAILURE;
+    }
   if (t1.NumElements() != t2.NumElements())
+    {
+    std::cout << "NumElements() differ!" << std::endl;
     return EXIT_FAILURE;
+    }
   for (unsigned int i = 0; i < t1.NumElements(); i++)
     if (t1.scalar<float>()(i) != t2.scalar<float>()(i))
+      {
+      std::cout << "scalar " << i << " differ!" << std::endl;
       return EXIT_FAILURE;
+      }
+  std::cout << "Tensors are equals :)" << std::endl;
   return EXIT_SUCCESS;
 }
 
