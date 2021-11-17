0c33ada4703899d432d98f35e5001f14dbf4a448
Remi Cresson <remi.cresson@irstea.fr>
2021-11-17 11:52:45 +0100

    ADD: test_utils.py

---
+++ test/test_utils.py
@@ -0,0 +1,41 @@
+import otbApplication
+
+
+def get_nb_of_channels(raster):
+    """
+    Return the number of channels in the input raster
+    :param raster: raster filename (str)
+    :return the number of channels in the image (int)
+    """
+    info = otbApplication.Registry.CreateApplication("ReadImageInfo")
+    info.SetParameterString("in", raster)
+    info.ExecuteAndWriteOutput()
+    return info.GetParameterInt('numberbands')
+
+
+def compare(raster1, raster2, tol=0.01):
+    """
+    Return True if the two rasters have the same contents in each bands
+    :param raster1: raster 1 filename (str)
+    :param raster2: raster 2 filename (str)
+    :param tol: tolerance (float)
+    """
+    n_bands1 = get_nb_of_channels(raster1)
+    n_bands2 = get_nb_of_channels(raster2)
+    if n_bands1 != n_bands2:
+        print("The images have not the same number of channels")
+        return False
+
+    for i in range(1, 1 + n_bands1):
+        comp = otbApplication.Registry.CreateApplication('CompareImages')
+        comp.SetParameterString('ref.in', raster1)
+        comp.SetParameterInt('ref.channel', i)
+        comp.SetParameterString('meas.in', raster2)
+        comp.SetParameterInt('meas.channel', i)
+        comp.Execute()
+        mae = comp.GetParameterFloat('mae')
+        if mae > tol:
+            print("The images have not the same content in channel {} "
+                  "(Mean average error: {})".format(i, mae))
+            return False
+    return True
