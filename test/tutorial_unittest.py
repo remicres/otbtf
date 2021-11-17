5a207f5f462400b29b49e87c120094ef12d40f85
Remi Cresson <remi.cresson@irstea.fr>
2021-11-17 15:22:28 +0100

    FIX: tests

--- test/tutorial_unittest.py
+++ test/tutorial_unittest.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import pytest
 import unittest
 import os
 from pathlib import Path
 import test_utils
+
+INFERENCE_MAE_TOL = 10.0  # Dummy value: we don't really care of the mae value but rather the image size etc
 
 
 def resolve_paths(path):
@@ -20,7 +22,7 @@ def run_command(command):
     :param command: the command to run
     """
     full_command = resolve_paths(command)
-    print("Running command {}".format(full_command))
+    print("Running command: \n\t {}".format(full_command))
     os.system(full_command)
 
 
@@ -31,11 +33,14 @@ def run_command_and_test_exist(command, file_list):
     :return True or False
     """
     run_command(command)
+    print("Checking if files exist...")
     for file in file_list:
+        print("\t{}".format(file))
         path = Path(resolve_paths(file))
         if not path.is_file():
             print("File {} does not exist!".format(file))
             return False
+        print("\tOk")
     return True
 
 
@@ -57,104 +62,114 @@ def run_command_and_compare(command, to_compare_dict, tol=0.01):
 
 class TutorialTest(unittest.TestCase):
 
+    @pytest.mark.order(1)
     def test_sample_selection(self):
         self.assertTrue(
             run_command_and_test_exist(
-                "otbcli_LabelImageSampleSelection "
-                "-inref $DATADIR/terrain_truth_epsg32654_A.tif "
-                "-nodata 255"
-                "-outvec $TMPDIR/outvec_A.gpkg", "outvec_A.gpkg"))
+                command="otbcli_LabelImageSampleSelection "
+                        "-inref $DATADIR/terrain_truth_epsg32654_A.tif "
+                        "-nodata 255 "
+                        "-outvec $TMPDIR/outvec_A.gpkg",
+                file_list=["$TMPDIR/outvec_A.gpkg"]))
         self.assertTrue(
             run_command_and_test_exist(
-                "otbcli_LabelImageSampleSelection "
-                "-inref $DATADIR/terrain_truth_epsg32654_B.tif "
-                "-nodata 255"
-                "-outvec $TMPDIR/outvec_B.gpkg", "outvec_B.gpkg"))
+                command="otbcli_LabelImageSampleSelection "
+                        "-inref $DATADIR/terrain_truth_epsg32654_B.tif "
+                        "-nodata 255 "
+                        "-outvec $TMPDIR/outvec_B.gpkg",
+                file_list=["$TMPDIR/outvec_B.gpkg"]))
 
+    @pytest.mark.order(2)
     def test_patches_extraction(self):
         self.assertTrue(
             run_command_and_compare(
-                "otbcli_PatchesExtraction "
-                "-source1.il $DATADIR/s2_stack.jp2 "
-                "-source1.out $TMPDIR/s2_patches_A.tif "
-                "-source1.patchsizex 16 "
-                "-source1.patchsizey 16 "
-                "-vec $TMPDIR/outvec_A.gpkg "
-                "-field class "
-                "-outlabels $TMPDIR/s2_labels_A.tif",
-                {"$DATADIR/s2_patches_A.tif": "$TMPDIR/s2_patches_A.tif",
-                 "$DATADIR/s2_labels_A.tif": "$TMPDIR/s2_labels_A.tif"}))
+                command="otbcli_PatchesExtraction "
+                        "-source1.il $DATADIR/s2_stack.jp2 "
+                        "-source1.out $TMPDIR/s2_patches_A.tif "
+                        "-source1.patchsizex 16 "
+                        "-source1.patchsizey 16 "
+                        "-vec $TMPDIR/outvec_A.gpkg "
+                        "-field class "
+                        "-outlabels $TMPDIR/s2_labels_A.tif",
+                to_compare_dict={"$DATADIR/s2_patches_A.tif": "$TMPDIR/s2_patches_A.tif",
+                                 "$DATADIR/s2_labels_A.tif": "$TMPDIR/s2_labels_A.tif"}))
         self.assertTrue(
             run_command_and_compare(
-                "otbcli_PatchesExtraction "
-                "-source1.il $DATADIR/s2_stack.jp2 "
-                "-source1.out $TMPDIR/s2_patches_B.tif "
-                "-source1.patchsizex 16 "
-                "-source1.patchsizey 16 "
-                "-vec $TMPDIR/outvec_B.gpkg "
-                "-field class "
-                "-outlabels $TMPDIR/s2_labels_B.tif",
-                {"$DATADIR/s2_patches_B.tif": "$TMPDIR/s2_patches_B.tif",
-                 "$DATADIR/s2_labels_B.tif": "$TMPDIR/s2_labels_B.tif"}))
+                command="otbcli_PatchesExtraction "
+                        "-source1.il $DATADIR/s2_stack.jp2 "
+                        "-source1.out $TMPDIR/s2_patches_B.tif "
+                        "-source1.patchsizex 16 "
+                        "-source1.patchsizey 16 "
+                        "-vec $TMPDIR/outvec_B.gpkg "
+                        "-field class "
+                        "-outlabels $TMPDIR/s2_labels_B.tif",
+                to_compare_dict={"$DATADIR/s2_patches_B.tif": "$TMPDIR/s2_patches_B.tif",
+                                 "$DATADIR/s2_labels_B.tif": "$TMPDIR/s2_labels_B.tif"}))
 
+    @pytest.mark.order(3)
     def test_generate_model1(self):
         run_command("git clone https://github.com/remicres/otbtf_tutorials_resources.git "
                     "$TMPDIR/otbtf_tuto_repo")
         self.assertTrue(
             run_command_and_test_exist(
-                "python $TMPDIR/otbtf_tuto_repo/01_patch_based_classification/models/create_model1.py "
-                "$TMPDIR/model1",
-                "$TMPDIR/model1/saved_model.pb"))
+                command="python $TMPDIR/otbtf_tuto_repo/01_patch_based_classification/models/create_model1.py "
+                        "$TMPDIR/model1",
+                file_list=["$TMPDIR/model1/saved_model.pb"]))
 
+    @pytest.mark.order(4)
     def test_model1_train(self):
         self.assertTrue(
             run_command_and_test_exist(
-                "otbcli_TensorflowModelTrain "
-                "-training.source1.il $DATADIR/s2_patches_A.tif "
-                "-training.source1.patchsizex 16 "
-                "-training.source1.patchsizey 16 "
-                "-training.source1.placeholder x "
-                "-training.source2.il $DATADIR/s2_labels_A.tif "
-                "-training.source2.patchsizex 1 "
-                "-training.source2.patchsizey 1 "
-                "-training.source2.placeholder y "
-                "-model.dir $TMPDIR/model1 "
-                "-training.targetnodes optimizer "
-                "-validation.mode class "
-                "-validation.source1.il $DATADIR/s2_patches_B.tif "
-                "-validation.source1.name x "
-                "-validation.source2.il $DATADIR/s2_labels_B.tif "
-                "-validation.source2.name prediction "
-                "-model.saveto $TMPDIR/model1/variables/variables"
+                command="otbcli_TensorflowModelTrain "
+                        "-training.source1.il $DATADIR/s2_patches_A.tif "
+                        "-training.source1.patchsizex 16 "
+                        "-training.source1.patchsizey 16 "
+                        "-training.source1.placeholder x "
+                        "-training.source2.il $DATADIR/s2_labels_A.tif "
+                        "-training.source2.patchsizex 1 "
+                        "-training.source2.patchsizey 1 "
+                        "-training.source2.placeholder y "
+                        "-model.dir $TMPDIR/model1 "
+                        "-training.targetnodes optimizer "
+                        "-validation.mode class "
+                        "-validation.source1.il $DATADIR/s2_patches_B.tif "
+                        "-validation.source1.name x "
+                        "-validation.source2.il $DATADIR/s2_labels_B.tif "
+                        "-validation.source2.name prediction "
+                        "-model.saveto $TMPDIR/model1/variables/variables",
+                file_list=["$TMPDIR/model1/variables/variables.index"]
             )
         )
 
+    @pytest.mark.order(5)
     def test_model1_inference_pb(self):
         self.assertTrue(
-            run_command_and_compare("otbcli_TensorflowModelServe "
-                                    "-source1.il $DATADIR/s2_stack.jp2 "
-                                    "-source1.rfieldx 16 "
-                                    "-source1.rfieldy 16 "
-                                    "-source1.placeholder x "
-                                    "-model.dir $TMPDIR/model1 "
-                                    "-output.names prediction "
-                                    "-out \"$TMPDIR/classif_model1.tif?&box=4000:4000:1000:1000\" uint8",
-                                    {"$DATADIR/classif_model1.tif": "$TMPDIR/classif_model1.tif"}))
+            run_command_and_compare(command="otbcli_TensorflowModelServe "
+                                            "-source1.il $DATADIR/s2_stack.jp2 "
+                                            "-source1.rfieldx 16 "
+                                            "-source1.rfieldy 16 "
+                                            "-source1.placeholder x "
+                                            "-model.dir $TMPDIR/model1 "
+                                            "-output.names prediction "
+                                            "-out \"$TMPDIR/classif_model1.tif?&box=4000:4000:1000:1000\" uint8",
+                                    to_compare_dict={"$DATADIR/classif_model1.tif": "$TMPDIR/classif_model1.tif"},
+                                    tol=INFERENCE_MAE_TOL))
 
+    @pytest.mark.order(6)
     def test_model1_inference_fcn(self):
         self.assertTrue(
-            run_command_and_compare("otbcli_TensorflowModelServe "
-                                    "-source1.il $DATADIR/s2_stack.jp2 "
-                                    "-source1.rfieldx 16 "
-                                    "-source1.rfieldy 16 "
-                                    "-source1.placeholder x "
-                                    "-model.dir $TMPDIR/model1 "
-                                    "-output.names prediction "
-                                    "-model.fullyconv on "
-                                    "-output.spcscale 4 "
-                                    "-out \"$TMPDIR/classif_model1.tif?&box=1000:1000:256:256\" uint8",
-                                    {"$DATADIR/classif_model1.tif": "$TMPDIR/classif_model1.tif"},
-                                    tol=0.1))
+            run_command_and_compare(command="otbcli_TensorflowModelServe "
+                                            "-source1.il $DATADIR/s2_stack.jp2 "
+                                            "-source1.rfieldx 16 "
+                                            "-source1.rfieldy 16 "
+                                            "-source1.placeholder x "
+                                            "-model.dir $TMPDIR/model1 "
+                                            "-output.names prediction "
+                                            "-model.fullyconv on "
+                                            "-output.spcscale 4 "
+                                            "-out \"$TMPDIR/classif_model1.tif?&box=1000:1000:256:256\" uint8",
+                                    to_compare_dict={"$DATADIR/classif_model1.tif": "$TMPDIR/classif_model1.tif"},
+                                    tol=INFERENCE_MAE_TOL))
 
 
 if __name__ == '__main__':
