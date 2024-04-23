806833ceb2cd7a2c88247aa2602662d0d7a574e7
Cresson Remi <remi.cresson@irstea.fr>
2024-04-23 15:47:52 +0200

    Update 2 files
    - /setup.py
    - /.gitlab-ci.yml

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="otbtf",
-    version="4.3.0",
+    version="4.3.1",
     author="Remi Cresson",
     author_email="remi.cresson@inrae.fr",
     description="OTBTF: Orfeo ToolBox meets TensorFlow",
