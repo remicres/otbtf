3cf7c42db655fb0000b0f34287405f2906ac08e1
Vincent Delbar <vincent.delbar@latelescop.fr>
2025-02-24 12:08:24 +0100

    ENH: use pyproject.toml, declare version in python module

--- otbtf/__init__.py
+++ otbtf/__init__.py
@@ -19,7 +19,9 @@
 """
 OTBTF python module
 """
-import pkg_resources
+
+__version__ = "5.0.0-rc2"
+
 try:
     from otbtf.utils import read_as_np_arr, gdal_open  # noqa
     from otbtf.dataset import Buffer, PatchesReaderBase, PatchesImagesReader, \
@@ -33,4 +35,3 @@ except ImportError:
 from otbtf.tfrecords import TFRecords  # noqa
 from otbtf.model import ModelBase  # noqa
 from otbtf import layers, ops  # noqa
-__version__ = pkg_resources.require("otbtf")[0].version
